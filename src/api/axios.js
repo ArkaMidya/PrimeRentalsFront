1049dc9f434e0669217aecd90a97650bbc3dfd38
ArMidya <arkamidya516@gmail.com>
2026-04-20 10:24:10 +0530

    axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000/api'
-    : 'https://primerentalsbackend-production.up.railway.app/api',
+    : 'https://primerentalsbackend-production.up.railway.app/',
 
   headers: {
     'Content-Type': 'application/json',
