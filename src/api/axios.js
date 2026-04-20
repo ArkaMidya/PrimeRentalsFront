f69d7960dc6ec699e23f036c255face39573e29e
ArMidya <arkamidya516@gmail.com>
2026-04-20 10:26:08 +0530

    OTP

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:5000/api'
-    : 'https://primerentalsbackend-production.up.railway.app/',
+    : 'https://primerentalsbackend-production.up.railway.app/api',
 
   headers: {
     'Content-Type': 'application/json',
